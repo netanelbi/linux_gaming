a696a76e31a4eafdad2e45e37face5f8fc8ee197
netanelbi <netanelbi1991@gmail.com>
2024-06-20 21:32:12 +0300

    small fix

--- shutdown-script.sh
+++ shutdown-script.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
+echo "shutdown started ***"
 
 # Variables
 DISK_NAME="vmdisk"
 VM_NAME="vm"
 ZONE="me-west1-c"  # Replace with your GCP zone
-
+sudo umount /mnt/$DISK_NAME
 # Detach the disk from the VM
+echo "Detaching disk $DISK_NAME from $VM_NAME"
 gcloud compute instances detach-disk $VM_NAME --disk=$DISK_NAME --zone=$ZONE
+if [ $? -eq 0 ]; then
+    echo "Successfully detached disk $DISK_NAME"
+    # Introduce a delay to ensure detachment is complete
+    sleep 5
 
-# Delete the disk
-gcloud compute disks delete $DISK_NAME --zone=$ZONE --quiet
+    # Delete the disk
+    echo "Deleting disk $DISK_NAME"
+    gcloud compute disks delete $DISK_NAME --zone=$ZONE --quiet
+    if [ $? -eq 0 ]; then
+        echo "Successfully deleted disk $DISK_NAME"
+    else
+        echo "Failed to delete disk $DISK_NAME"
+    fi
+else
+    echo "Failed to detach disk $DISK_NAME"
+fi
+
+echo "shutdown successful"
